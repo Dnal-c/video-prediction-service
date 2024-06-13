9619c92bdc011e1666459d4c223a3508b271479c
Николай Яблочкин <applenik41@gmail.com>
2024-06-13 23:34:22 +0300

    save predictions

--- global_env.py
+++ global_env.py
@@ -13,12 +13,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # Если запуск идет с CPU, то необходимо float32 или float16, на GPU можно пробовать int8, float16, float32
 WHISPER_COMPUTE_TYPE = "int8" if DEVICE == "gpu" else "float32"
 
-
-BASE_DIR_PATH = '/Users/apple/Documents/projects/hackaton-lct-2024/video-prediction-service'
+BASE_DIR_PATH = '/Users/dandi/PycharmProjects/etl-process/'
 MODEL_CAPTION_PATH = BASE_DIR_PATH + '/models/captioning'  # нужен для локальной работы с image captioning
 MODEL_TRANSLATOR_PATH = BASE_DIR_PATH + '/models/translator'  # нужен для локальной работы с automatic speech recogni
 
-
-TEMP_DIRECTORY_PATH = '/Users/apple/Documents/projects/hackaton-lct-2024/video-prediction-service/images'  # Параметр, отвечающий за то, куда будем складывать файлы
+TEMP_DIRECTORY_PATH = '/Users/dandi/Documents/hack/temp/'
 
 SAVING_FRAMES_PER_SECOND = .25  # Параметр, отвечающий за то, сколько фреймов с видео резать за секунду для обработки
